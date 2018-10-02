9e803bf1be2704fb9ffe49d6a0e33b017ace0205
PillowCroco <dnjf55@gmail.com>
2018-10-02 16:32:35 +0900

    image augmented

--- app/src/main/java/com/virgin/trial/rsp_game/MainActivity.java
+++ app/src/main/java/com/virgin/trial/rsp_game/MainActivity.java
@@ -3,7 +3,6 @@ package com.virgin.trial.rsp_game;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
